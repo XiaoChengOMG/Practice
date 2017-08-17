275d64b678eaaf193866eced0720838f4817b1f1
XiaoChengOMG <444059482@qq.com>
2017-08-17 20:40:51 +0800

    modify orm.py

--- www/orm.py
+++ www/orm.py
@@ -7,6 +7,7 @@ __author__ = ""
 '''
 
 import asyncio,logging
+logging.basicConfig(level=logging.INFO)
 import aiomysql
 
 def log(sql,args=()):
@@ -27,3 +28,17 @@ async def create_pool(loop,**kw):
         minsize=kw.get('minsize',1),
         loop=loop
     )
+
+async def select(sql,args,size=None):
+    log(sql,args)
+    global _pool
+    async with _pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?','%s'),args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
